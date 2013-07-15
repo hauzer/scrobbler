f5688b2292c82173ae216c1a38cbbccd5c27d4b4
hauzer <hauzer@localhost>
2013-07-15 20:02:25 +0200

    Renamed to scrobbler. Added setup.py.

---
+++ setup.py
@@ -0,0 +1,35 @@
+#
+# A Last.fm scrobbler and a now-playing status updater.
+# Copyright (C) 2013  Nikola "hauzer" Vukosavljević
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+from setuptools import setup, find_packages
+
+
+setup(name              = "scrobbler",
+      version           = "1.0.0",
+      packages          = find_packages(),
+      install_requires  = ["lfm"],
+
+      author        = "Никола \"hauzer\" Вукосављевић",
+      author_email  = "hauzer@gmx.com",
+      description   = "A Last.fm scrobbler and a now-playing status updater.",
+      # long_description = "",
+      license       = "GPLv3",
+      url           = "https://bitbucket.org/hauzer/scrobble/",
+      download_url  = "https://bitbucket.org/hauzer/scrobble/downloads",
+      )
