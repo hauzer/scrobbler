1dd0b8b14f0d925b4e5605ea93a31bb400833bf7
hauzer <hauzer@localhost>
2013-07-24 15:59:10 +0200

    Fixed a bug where when using -f the timestamp would always be 'now'. Made -s mandatory in the 'scrobble' command.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 
 
 setup(name              = "scrobblerh",
-      version           = "1.0.0",
+      version           = "1.0.1",
       scripts           = ["scrobbler.py"],
       install_requires  = ["appdirs", "lfmh"],
 
